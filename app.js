58804a396be030a31af839fd78fd412b7029cbed
OkamiAka <nils.caqueux@gmail.com>
2023-05-09 11:10:28 +0200

    connection

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 5000;
+
+const database = mysql.createPool({
+  host: process.env.DB_HOST, // address of the server
+  port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the APP_PORT !
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+const HANDLER = (req, res) => {
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      // wait for it
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+app.get("/", welcome);
+app.get("/api/movies", HANDLER);
+app.post("/api/movies", postMovie);
